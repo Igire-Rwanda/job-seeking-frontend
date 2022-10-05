35c37b151b6470fd5f35eb3d3de0540014e5d198
Lydie <gatarelydie370@gmail.com>
2022-10-05 17:29:36 +0200

    added Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
+import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Employers from './pages/Employers';
 import Navbar from './components/Navbar';
+import JobSeekers from './pages/JobSeekers';
+import PostJob from './pages/PostJob';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Footer from './pages/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-    </div>
+    <BrowserRouter>
+   <Routes>
+   <Route exact path='/' element={<Home/>}/>
+
+    <Route path='/Home' element={<Home/>}/>
+    <Route path='/Employers' element={<Employers/>}/>
+    <Route path='/JobSeekers' element={<JobSeekers/>}/>
+    <Route path='/PostJob' element={<PostJob/>}/>
+    <Route path='/SignIn' element={<SignIn/>}/>
+    <Route path='/SignUp' element={<SignUp/>}/>
+    <Route path='/Footer' element={<Footer/>}/>
+   </Routes>
+    </BrowserRouter>
+    
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,23 @@
 import React from 'react'
+import {link, NavLink} from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div>
-<nav className='shadow-md w-full fixed top-0 left-0'>
+<nav className='shadow-md w-full fixed top-0 left-0 '>
       <div className='md:flex bg-white py-4 items-cennter justify-between md:px-20'>
-         <div className='font-bold text-2xl cursor-pointer flex items-center font-[poppins] text-gray-800'>
-           <span className='text-3xl text-indigo-600 mr-1 pt-2'>
-           </span>
-           Jolinker
+         <div className=' md:pl-0 pl-9 font-bold text-2xl cursor-pointer flex items-center font-[poppins] text-gray-800'>
+           <span className='text-emerald-700 mr-1 '>
+           Jo</span> <h1>linker</h1>
          </div>
-         <ul className='md:flex md:pb-0 pb-2 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in'>
-          <li className='mx-4 hover:text-teal-500 duration-500 cursor-pointer text-l md:ml-8 md:my-0 my-7'>Home</li>
-          <li className='mx-4 hover:text-teal-500 duration-500 cursor-pointer text-l md:ml-8 md:my-0 my-7'>Employers</li>
-          <li className='mx-4 hover:text-teal-500 duration-500 cursor-pointer text-l md:ml-8 md:my-0 my-7'>Job seekers</li>
-          <li className='mx-4 hover:text-teal-500 duration-500 cursor-pointer text-l md:ml-8 md:my-0 my-7'>Post a Job</li>
-          <button className='bg-yellow-400	rounded-md py-2 px-6 md:ml-8 text-white'>Sign Up</button>
+         <ul className='md:flex md:pb-0 pb-1 py-3 absolute md:static bg-white md:z-auto z-[-1] left-0 md:w-auto md:pl-0 pl-5'>
+       <NavLink to='/'><li className='mx-4 hover:text-teal-500 duration-500 cursor-pointer text-l md:ml-8 md:my-0 my-5 py-3 px-5'>Home</li></NavLink>  
 
+        <NavLink to='/Employers'><li className='mx-4 hover:text-teal-500 duration-500 cursor-pointer text-l md:ml-8 md:my-0 my-5 py-3 px-5'>Employers</li></NavLink> 
+         <NavLink to='/jobSeeker'><li className='mx-4 hover:text-teal-500 duration-500 cursor-pointer text-l md:ml-8 md:my-0 my-5 py-3 px-5'>Job seekers</li></NavLink> 
+        <NavLink to='/PostJob'><li className='mx-4 hover:text-teal-500 duration-500 cursor-pointer text-l md:ml-8 md:my-0 my-5 py-3 px-5'>Post a Job</li></NavLink>  
+         <NavLink to='/SignIn'><li className='mx-4  px-5 py-3 cursor-pointer'>Sign In</li> </NavLink> 
+        <NavLink to='/SignUp'><button className=' mx-8 bg-yellow-400	rounded-md py-2 px-6 md:ml-8 text-white'>Sign Up</button></NavLink> 
 
          </ul>
 

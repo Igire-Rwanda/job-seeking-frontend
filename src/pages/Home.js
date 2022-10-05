5123300f7b1caa847ed75759af7ac7ecd5d7b2f7
Lydie <gatarelydie370@gmail.com>
2022-10-05 17:29:36 +0200

    added Navbar

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+  return (
+    
+    <div>
+      <Navbar/>
+      
+<div className='mt-20'>
+It's Easy To Find Your <span className='text-teal-500' >Dream Jo</span>
+</div>
+
+    </div>
+    
+  )
+}
+
+export default Home

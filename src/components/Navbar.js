106c5f06ccf5f6f493d0e820a02a88d4b42bed14
Lydie <gatarelydie370@gmail.com>
2022-10-05 17:44:33 +0200

    added navbar and created routes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div>
 <nav className='shadow-md w-full fixed top-0 left-0 '>
-      <div className='md:flex bg-white py-4 items-cennter justify-between md:px-20'>
+      <div className='md:flex bg-white py-2 items-cennter justify-between md:px-20 mt:10'>
          <div className=' md:pl-0 pl-9 font-bold text-2xl cursor-pointer flex items-center font-[poppins] text-gray-800'>
            <span className='text-emerald-700 mr-1 '>
            Jo</span> <h1>linker</h1>
@@ -14,7 +14,7 @@ const Navbar = () => {
        <NavLink to='/'><li className='mx-4 hover:text-teal-500 duration-500 cursor-pointer text-l md:ml-8 md:my-0 my-5 py-3 px-5'>Home</li></NavLink>  
 
         <NavLink to='/Employers'><li className='mx-4 hover:text-teal-500 duration-500 cursor-pointer text-l md:ml-8 md:my-0 my-5 py-3 px-5'>Employers</li></NavLink> 
-         <NavLink to='/jobSeeker'><li className='mx-4 hover:text-teal-500 duration-500 cursor-pointer text-l md:ml-8 md:my-0 my-5 py-3 px-5'>Job seekers</li></NavLink> 
+         <NavLink to='/JobSeeker'><li className='mx-4 hover:text-teal-500 duration-500 cursor-pointer text-l md:ml-8 md:my-0 my-5 py-3 px-5'>Job seekers</li></NavLink> 
         <NavLink to='/PostJob'><li className='mx-4 hover:text-teal-500 duration-500 cursor-pointer text-l md:ml-8 md:my-0 my-5 py-3 px-5'>Post a Job</li></NavLink>  
          <NavLink to='/SignIn'><li className='mx-4  px-5 py-3 cursor-pointer'>Sign In</li> </NavLink> 
         <NavLink to='/SignUp'><button className=' mx-8 bg-yellow-400	rounded-md py-2 px-6 md:ml-8 text-white'>Sign Up</button></NavLink> 

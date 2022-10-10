16e4122868934aa5d4dfee308d0086b362b92bf5
Ingabire <imimaculee@gmail.com>
2022-10-10 08:20:04 +0200

    add jobdashboard

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
+
+
 import './App.css';
-// import Navbar from './components/Navbar';
-import Footer from './components/footer';
-// import JobSearching from './components/jobs';
+import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import Home from './pages/Home';
+import JobSearching from './Views/jobs';
+
+// import SignIn from './pages/SignIn';
+// import SignUp from './pages/SignUp';
+import HomeLayout from './components/homeLayout';
+import JobCards from './components/jobCard';
+// import FindJobs from './pages/FindJobs';
+// import HireTalent from './pages/HireTalent';
+
 function App() {
   return (
-    <div className="App">
-      {/* <Navbar/> */}
-      <Footer/>
-      {/* <JobSearching/> */}
-    </div>
+    <BrowserRouter>
+    <HomeLayout>
+   <Routes>
+    
+   {/* <Route exact path='/' element={<Home/>}/> */}
+
+    {/* <Route path='/Home' element={<Home/>}/> */}
+    {/* <Route path='/FindJobs' element={<FindJobs/>}/> */}
+    {/* <Route path='/HireTalent' element={<HireTalent/>}/> */}
+    {/* <Route path='/SignIn' element={<SignIn/>}/> */}
+    {/* <Route path='/SignUp' element={<SignUp/>}/> */}
+    <Route path='/cards' element={<JobCards/>}/>
+    <Route path='/jobs' element={<JobSearching/>}/>
+    
+   </Routes>
+   </HomeLayout>
+    </BrowserRouter>
+    
   );
 }
 
---
+++ src/components/jobCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const JobCards = () => {
+    return (
+        <>
+         
+        <div className='jobCard'>
+           <p className="new">new</p>
+           <h1 className="h1">SOFTWARE DEVELOPMENT ENGINEER I</h1> 
+           <h4 className="h4">Amazon.com Services LLc</h4>
+           <p className="h5">Remote</p>
+           <button className="Btn1 h5">Full Time</button><br/><br/>
+           <p>Hiring Multiple Candidates</p>
+           <p>It's not the secret that Amazon relies on its technology to deliver million of
+            packages everyday to it's customers - everyday- at a low cost.
+           </p> <br/>
+           <button className="btn3">Hiring 167 Candidates</button>
+        </div>
+     
+        </>
+    )
+}
+export default JobCards

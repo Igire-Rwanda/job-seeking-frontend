be93da1c79bcf2c376053ce87620755eb20d590b
Ingabire <imimaculee@gmail.com>
2022-10-17 16:28:45 +0200

    adding admin routes

--- src/App.js
+++ src/App.js
@@ -1,45 +1,31 @@
 
-
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import './App.css';
 import './index.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
- import Home from './pages/Home';
-import JobSearching from './Views/jobs';
-import HomeLayout from './components/homeLayout';
-import JobCards from './components/jobCard';
-import AdminLayout from './Views/Dshboard/AdminDash';
-import UserLayout from './Views/Dshboard/Userdashboard';
-import DataTable from './Views/Dshboard/managerUsers';
- import FindJobs from './pages/FindJobs';
- import HireTalent from './pages/HireTalent';
-import DashLayout from './components/reusableComponents/Layout'
+import Routes from "./routes";
+import AdminRoutes from './routes/admin';
 
+// import { Provider } from "react-redux";
+// import store from "./redux/stores";
+ function App() {
+    return(
+      <>
+        {/*  <Provider store={store}> */}
 
-
-
-function App() {
-  return (
-    <BrowserRouter>
-    <HomeLayout>
-   <Routes>
+      <Router>
+        <AdminRoutes></AdminRoutes>
+       </Router>
+       
+       {/* <Router>
+    <Routes></Routes>
    
-         
-            
+</Router>   */}
 
-    <Route path='/Home' element={<Home/>}/>
-    <Route path='/FindJobs' element={<FindJobs/>}/>
-    <Route path='/dashb' element={<DataTable/>}/>
-    <Route path='/Admindash' element={<AdminLayout/>}/>
-    <Route path='/Userdash' element={<UserLayout/>}/>
-    <Route path='/cards' element={<DashLayout><JobCards/></DashLayout>}/>
-    <Route path='/jobs' element={<JobSearching/>}/>
-    <Route path='/managerUser' element={<DataTable/>}/>
-   </Routes>
-   </HomeLayout>
-  
-    </BrowserRouter>
+        {/*  </Provider> */}
+        </>
+        );
     
-  );
-}
-
+    
+};
 export default App;

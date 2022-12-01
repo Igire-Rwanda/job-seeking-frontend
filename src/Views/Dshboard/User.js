2604149374ddf00368f6df97e6db4ae303330db5
salimaNibareke <nibarekesalima@gmail.com>
2022-12-01 11:16:03 +0200

    redux

--- src/Views/Dshboard/User.js
+++ src/Views/Dshboard/User.js
@@ -12,43 +12,10 @@ import View from './View';
 
 import { useDispatch, useSelector } from "react-redux";
 
-import { getAllProductAction } from '../../redux/product/actions';
+// import { getAllProductAction } from '../../redux/product/actions';
 
 
-const TableData = [{
-  "name": "Snow Jon",
-  "post": " Back-end developer",
-  "date": "Sep 12th, 2021",
-  "delete": "delete",
-  "candidates": "View",
-  "gender": "Female",
-  "ip_address": "68.54.224.213"
-}, {
-  "name": "Lannister Cersei",
-  "post": " Front-end developer",
-  "date": "Dec 12th, 2021",
-  "delete": "delete",
-  "candidates": "View",
-  "gender": "Female",
-  "ip_address": "32.251.148.67"
-}, {
-  "name": "Lannister Jaime",
-  "post": " Software Engineer",
-  "date": "Jan 12th, 2021",
-  "delete": "delete",
-  "candidates": "View",
-  "gender": "Agender",
-  "ip_address": "91.186.214.106"
-}, 
-{
-  "name": "Salima cool",
-  "post": " Software Engineer",
-  "date": "Jul 12th, 2021",
-  "delete": "delete",
-  "candidates": "View",
-  "gender": "Female",
-  "ip_address": "211.8.50.110"
-}]
+
 
 
 
@@ -66,15 +33,93 @@ const style = {
 };
 
 export default function ManagPostedJobs() {
+  const [TableData ,setTableData ] = useState ([{
+    "id":"1",
+    "name": "Snow Jon",
+    "post": " Back-end developer",
+    "date": "Sep 12th, 2021",
+    "delete": "delete",
+    "candidates": "View",
+    "gender": "Female",
+    "ip_address": "68.54.224.213"
+  }, {
+    "id":"2",
+    "name": "Lannister Cersei",
+    "post": " Front-end developer",
+    "date": "Dec 12th, 2021",
+    "delete": "delete",
+    "candidates": "View",
+    "gender": "Female",
+    "ip_address": "32.251.148.67"
+  }, {
+    "id":"3",
+    "name": "Lannister Jaime",
+    "post": " Software Engineer",
+    "date": "Jan 12th, 2021",
+    "delete": "delete",
+    "candidates": "View",
+    "gender": "Agender",
+    "ip_address": "91.186.214.106"
+  }, 
+  {
+    "id":"4",
+    "name": "Salima cool",
+    "post": " Software Engineer",
+    "date": "Jul 12th, 2021",
+    "delete": "delete",
+    "candidates": "View",
+    "gender": "Female",
+    "ip_address": "211.8.50.110"
+  },
+{
+  key:"5",
+  title:"Actions",
+  render:(record)=>{
+    return(
+      <>
+      <delete onClick ={()=>{
+onDelete (record)
+      }}
+      />
+        
+      </>
+    );
+  }
+}
+]
+  )
+
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const dispatch = useDispatch();
+  
   // const { TableData, isFetching } = useSelector((state) => state.product)
   useEffect(() => {
-    getAllProductAction()(dispatch);
+    // getAllProductAction()(dispatch);
   }, []);
 
+const onAddNew = ()=>{
+  const randomNumber =parseInt(Math.random()+5);
+const addNew ={
+  id: randomNumber ,
+  name: "snow jon",
+  post: " Software Engineer",
+  date: "Jul 12th, 2021",
+  delete: "delete",
+  candidates: "View",
+  gender: "Female",
+  ip_address: "211.8.50.110"
+}
+setTableData(pre=>{
+  return [...pre,addNew]
+})
+}
+const onDelete=(record)=>{
+  setTableData(pre=>{
+    pre.filter()
+  })
+}
 
   return (
     <div>
@@ -94,8 +139,9 @@ export default function ManagPostedJobs() {
       <TableContainer component={Paper}>
         <Table aria-label='simple table'>
           <TableHead>
-
+          <Button onClick={onAddNew}>Add New</Button>
             <TableRow>
+            <TableCell >ID</TableCell>
               <TableCell >Names</TableCell>
               <TableCell align='center'>Post Name</TableCell>
               <TableCell align='center'>Date</TableCell>
@@ -109,7 +155,7 @@ export default function ManagPostedJobs() {
               <TableRow
                 key={row.id}
                 sx={{ '&:last-child td, &:last-child': { border: 0 } }}>
-
+              <TableCell >{row.id}</TableCell>
                 <TableCell >{row.name}</TableCell>
                 <TableCell align='center'>{row.post}</TableCell>
                 <TableCell align='center'>{row.date}</TableCell>

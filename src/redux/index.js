d6b07e4149422ce28c3c7333fd5585c8d284a1b2
mimi7427 <umudede22@gmail.com>
2022-12-06 11:10:30 +0200

    redux changes

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import auth from "./auth";
 import user from "./user";
 import job from './job';
+import profile from './profile';
 const store = configureStore({
     reducer: {
         auth:auth,
